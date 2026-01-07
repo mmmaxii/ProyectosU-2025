7d256f167edf032a8e840d61d62be7f9d2d2450e
mmmaxii <mvalderrav@uc.cl>
2026-01-07 20:45:35 -0300

    Analisis de la galaxia ngc5972, Grafico de momentos, teselacion de voronoio, mapa de decremento de balmber, diagrama bpt y graficos de razones entre componentes quimicos. Todo esto mediante una IFU (Integral Field Units). Una de las mejores experiencias, tanto por el contenido como por el codigo.

---
+++ Astrofisica Experimental/Experiencia 2/Avance 2/.ipynb_checkpoints/Correcion_polvo_ayuda-checkpoint.py
@@ -0,0 +1,13 @@
+import numpy as np
+import extinction
+
+#https://iopscience.iop.org/article/10.1088/0004-637X/763/2/145/pdf
+
+ObservedHa_Hb = 5 #Razon medida de Halpha sobre Hbeta. Notar que son Luminosidades. 
+
+#Esto compara la razón de luminosidad con la razón teorica de 2.86 y lo compara con la curva de extinción de ccm89 para las longitudes de onda de Ha y Hb
+EB_V = (2.5/(extinction.calzetti00(np.array([4861.0]), 1, 1)-extinction.calzetti00(np.array([6563.0]), 1, 1)))*np.log10(ObservedHa_Hb/2.86)
+
+#Esta función calcula los factores de correción para una longitud de onda dada usando el EB_V calculado arriba. 
+# Esta función devuelve un factor, digamos 1.2x. Entonces el flujo corregido es el flujo observado multiplicado por 1.2. 
+extinction.remove(extinction.calzetti00(np.array([6563.0]), EB_V, 1), [1])
